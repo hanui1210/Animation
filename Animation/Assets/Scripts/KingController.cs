a76dfa11457ce9c4e95dbe21bcb9725e599b8013
DESKTOP-IJGTN5K\ss <hanuiii1210@gmail.com>
2024-08-25 21:57:42 +0900

    이동, 공격, 데미지 출력 / 공격끝나면 정지 안됨 & pig모션 안바뀜

--- Animation/Assets/Scripts/KingController.cs
+++ Animation/Assets/Scripts/KingController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,65 +8,115 @@ public class KingController : MonoBehaviour
 {
     public Animator animator;
     public Rigidbody2D rb2D;
-    public float moveSpeed = 1;
-    private float time;
-    private float attackSpeed = 0.3f;
-    private bool istime;
+    public float moveSpeed;
+    private bool isMoveStop;
+    private bool isAttacktime;
+    private float curAttackTime = 0f; // ∞£∞› ≈∏¿Ã∏”
+    private float attackTime = 0.6f; // ∞£∞› Ω√∞£
+    private bool isAttack;
 
-    
+    public GameObject kingGo;
+    public GameObject pigGo;
+    public KingController kingController;
+    public PigContorller pigContorller;
+
+    public int kingDamage;
+    public int pigHp;
+    private int pigMaxHp;
+    private Transform pigTrans;
+
+    private Vector3 kingPos;
 
     private void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-       
+        kingController = kingGo.GetComponent<KingController>();
+        pigContorller = pigGo.GetComponent<PigContorller>();
+        animator = kingController.GetComponent<Animator>();
+        this.pigTrans = GameObject.Find("Pig").transform;
+        Debug.Log($"≈∑ ∞¯∞›∑¬: {kingDamage}");  
     }
 
     // Update is called once per frame
     public void Update()
     {
+        KingMove();            
+       
+    }
+    // ∏ÿ√Á = ¥ﬁ∑¡ / ∏ÿ√Áº≠ ∂ß∑¡
+    // ¥ﬁ∑¡ = ∏ÿ√Á / ¥ﬁ∏Æ¥Ÿ ∏ÿ√Áº≠ ∂ß∑¡
+    // ∂ß∑¡ = ∏ÿ√Á / ∏ÿ√ﬂ∞Ì ¥ﬁ∑¡
+    public void KingMove()
+    {
         float h = Input.GetAxisRaw("Horizontal");
-
-        
 
         if (h == 0f)
         {
             // ∏ÿ√Á¿÷¿Ω
             animator.SetInteger("state", 0);
             KingAttack();
-
         }
 
-        else
-
+        else if (h != 0)
         {
+            animator.SetInteger("state", 1);
             this.rb2D.velocity = new Vector2(h * moveSpeed, 0);
             this.transform.localScale = new Vector3(h, 1, 1);
-            animator.SetInteger("state", 1);
             KingAttack();
-            Debug.Log(this.rb2D.velocity);
-            
         }
-
-        Check();
     }
-    // ∏ÿ√Á = ¥ﬁ∑¡ / ∏ÿ√Áº≠ ∂ß∑¡
-    // ¥ﬁ∑¡ = ∏ÿ√Á / ¥ﬁ∏Æ¥Ÿ ∏ÿ√Áº≠ ∂ß∑¡
-    // ∂ß∑¡ = ∏ÿ√Á / ∏ÿ√ﬂ∞Ì ¥ﬁ∑¡
 
     public void KingAttack()
     {
-
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            this.rb2D.velocity = new Vector2(0, 0);
+            this.curAttackTime += Time.deltaTime;
+            this.isAttacktime = true;
+         }
+        if (curAttackTime > 0 && curAttackTime < 0.6f && Input.GetKeyDown(KeyCode.Return))
+        {
             animator.SetInteger("state", 2);
-            Debug.Log("∞¯∞›¿Ã ≥°≥µΩ¿¥œ¥Ÿ");
-            Debug.Log(this.rb2D.velocity);
-
+            curAttackTime = attackTime;
+            this.isAttacktime = false;
+            this.rb2D.velocity = Vector2.zero;
+            CheckAndAttack();
+            Debug.Log("King Attack");
+        }
+        else if(curAttackTime >=0.6f && Input.GetKeyDown(KeyCode.Return))
+        {
+            this.curAttackTime += Time.deltaTime;
+            this.rb2D.velocity = Vector2.zero;
+            animator.SetInteger("state", 2);
+            CheckAndAttack();
+            Debug.Log("King Attack");
         }
     }
 
-   
+    public void KingDamage(int pigHp, int pigMaxHp, int kingDamage)
+    {
+
+        pigContorller.PigDead();
+        this.pigHp = this.pigContorller.pigMaxHp;
+        this.pigMaxHp = this.pigContorller.pigMaxHp;
+        this.pigContorller.pigHp -= this.kingDamage;
+        Debug.Log($"Pig¿« √º∑¬: {this.pigContorller.pigHp}/{this.pigContorller.pigMaxHp}");
+    }
+  
+    public void CheckAndAttack()
+    {
+        this.kingPos = this.transform.position;
+        Debug.Log($"kingPos: {kingPos}");
+        float dirX = pigTrans.transform.position.x - this.kingGo.transform.position.x;
+        
+        if(dirX > 1.30f && dirX > -7.80f)
+        {
+            Debug.Log($"dirX: {dirX}");
+            Debug.Log("∆»¿Ã¬™æ∆");
+        }
+        else
+        {
+            KingDamage(pigHp,pigMaxHp,kingDamage);
+        }
+    }
+
 
 }
--- Animation/Assets/Scripts/PigContorller.cs
+++ Animation/Assets/Scripts/PigContorller.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PigContorller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
+    public Collider2D Collider2D;
+  
+    public int pigHp;
+    public int pigMaxHp;
+    public GameObject kingGo;
+    public KingController kingController;
+    public PigContorller pigContorller;
+    public bool isPigDead;
+   
+
     void Start()
     {
-        
+        kingController = kingGo.GetComponent<KingController>();
+        animator = pigContorller.GetComponent<Animator>();
+        this.pigHp = 100;
+        this.pigMaxHp = 100;
+        Debug.Log($"PigÀÇ Ã¼·Â : {this.pigHp}/{this.pigMaxHp}");      
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
+    {      
+        PigDead();
     }
+
+    public void PigDead()
+    {
+        if( isPigDead ) return;
+        float pigH = Input.GetAxisRaw("Horizontal");
+        if(this.kingController.pigHp > 0)
+        {
+            animator.SetInteger("pigstate", 0);
+        }
+        else if(this.kingController.pigHp >= 0)
+        {
+            animator.SetInteger("pigstate", 1);
+        }
+        else if (this.kingController.pigHp < 0)
+        {
+            animator.SetInteger("pigstate", 2);
+            isPigDead = true;
+            Debug.Log("Pig Dead");
+        }
+    }
+    
 }
